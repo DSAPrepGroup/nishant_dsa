66041f3b812fa308fbf854abbd8729c2f84affbb
Neelam Thakur <neelam@Neelams-Air.lan>
2025-03-04 20:16:01 -0600

    tree easy

---
+++ Trees/maxDepthofBinaryTree.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        left = 1 + self.maxDepth(root.left)
+        right = 1 + self.maxDepth(root.right)
+
+        ans = max(left,right)
+        return ans
+        
